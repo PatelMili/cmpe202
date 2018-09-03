0f6efb49611258b89bfe5bbe2338eca2f778ab83
PatelMili <mili.patel@sjsu.edu>
2018-09-03 01:07:13 -0700

    initial modifications

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -4,40 +4,132 @@ public class GumballMachine
 
     private int num_gumballs;
     private boolean has_quarter;
+    private int model_no;
 
-    public GumballMachine( int size )
+    public GumballMachine( int size,int model_no)
     {
         // initialise instance variables
         this.num_gumballs = size;
         this.has_quarter = false;
+        this.model_no = model_no;
     }
 
-    public void insertQuarter(int coin)
+    // public void checkModel(int model_no)
+    // {
+        // switch(model_no){
+
+            // case 1: //model number one 
+            // break;
+            // case 2: //model number two
+            // break;
+            // case 3: //model number three
+            // break;
+            // default://in case of different number 
+
+        // }
+
+    // }
+
+    public void insertQuarter(int coin[])
     {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
+        //System.out.println( "model no:"+ model_no ) ;
+        switch(model_no){
+
+            case 1: //model number one 
+                   // System.out.println( "model no 1:"+ model_no ) ;
+                   if(coin.length == 1){
+                       
+                        if ( coin[0] == 25)
+                            this.has_quarter = true ;
+                        else 
+                            this.has_quarter = false ;
+                            
+                    }else{
+                     this.has_quarter = false;   
+                    }
+                    
+                    break;
+            case 2: //model number two
+                    //System.out.println( "model no 2:"+ model_no +"coin length"+coin.length ) ;
+                    
+                    if(coin.length == 2){
+                        if ( coin[0] == 25 && coin[1]== 25)
+                            this.has_quarter = true ;
+                        else 
+                            this.has_quarter = false ;
+                    }
+                    else{
+                        this.has_quarter = false;
+                    }
+                
+                    
+                    break;
+            case 3: //model number three
+            
+                   // System.out.println( "model no 3:"+ model_no ) ;
+                    int total=0;
+                    int flag = 0;
+                     if(coin.length >0)
+                        {
+                           for(int i=0;i<coin.length;i++){
+                            
+                                //System.out.println( "coin[i]"+ coin[i] ) ;
+                                if(coin[i]==25 || coin[i]==10 || coin[i]==5){
+                                    
+                                    flag=1;
+                                    //System.out.println( "flag is "+ flag ) ;
+                                
+                                }else{
+                                    flag=0;
+                                    //System.out.println( "flag is "+ flag ) ;
+                                    break;
+                                }
+                                
+                                
+                            }
+                           if(flag==1){
+                            
+                                for(int i=0;i<coin.length;i++){
+                            
+                                    total = total+ coin[i];
+                                
+                                }
+                            //System.out.println( "total"+ total ) ;
+                                if(total==50){
+                                    this.has_quarter = true ;
+                                }
+                            }
+                            
+                            
+                        
+                    }
+                    else 
+                        this.has_quarter = false ;
+            break;
+            default://in case of different number 
+
+        }
+
     }
-    
+
     public void turnCrank()
     {
-    	if ( this.has_quarter )
-    	{
-    		if ( this.num_gumballs > 0 )
-    		{
-    			this.num_gumballs-- ;
-    			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
-    		}
-    		else
-    		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
-    		}
-    	}
-    	else 
-    	{
-    		System.out.println( "Please insert a quarter" ) ;
-    	}        
+        if ( this.has_quarter )
+        {
+            if ( this.num_gumballs > 0 )
+            {
+                this.num_gumballs-- ;
+                this.has_quarter = false ;
+                System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
+            }
+            else
+            {
+                System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
+            }
+        }
+        else 
+        {
+            System.out.println( "Please insert a quarter" ) ;
+        }        
     }
 }
--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -2,21 +2,30 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+    public static void main(String[] args) {
+        
+        //second parameter is for model number, 1 is gumball machine type one, 2 is for gumball machine type 2 and 3 is for gumball machine type 3
+        
+        GumballMachine gumballMachine = new GumballMachine(5,2);
 
-		System.out.println(gumballMachine);
+        int []array1= {25};
+        int []array2={25,25};  
+        int []array3={25,10,10,5};
+        //i parameter is machine one, two paramter is machine two[],
+        
+        
+        System.out.println(gumballMachine);
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
+        gumballMachine.insertQuarter( array1 );
+        gumballMachine.turnCrank();
 
-		System.out.println(gumballMachine);
+        System.out.println(gumballMachine);
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
+        gumballMachine.insertQuarter( array2 );
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter( array3 );
+        gumballMachine.turnCrank();
 
-		System.out.println(gumballMachine);
-	}
+        System.out.println(gumballMachine);
+    }
 }
