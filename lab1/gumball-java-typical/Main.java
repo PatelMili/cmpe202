f4998cda012846a5debdee74e46252e938d3a209
PatelMili <mili.patel@sjsu.edu>
2018-09-03 02:10:56 -0700

    adding the second problem folder and some other modifications in first folder

--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
         
         //second parameter is for model number, 1 is gumball machine type one, 2 is for gumball machine type 2 and 3 is for gumball machine type 3
-        
-        GumballMachine gumballMachine = new GumballMachine(5,2);
+        GumballMachine gumballMachine1 = new GumballMachine(5,1);
+        GumballMachine gumballMachine2 = new GumballMachine(5,2);
+        GumballMachine gumballMachine3 = new GumballMachine(5,3);
 
         int []array1= {25};
         int []array2={25,25};  
@@ -14,18 +15,20 @@ public class Main {
         //i parameter is machine one, two paramter is machine two[],
         
         
-        System.out.println(gumballMachine);
+        System.out.println(gumballMachine1);
 
-        gumballMachine.insertQuarter( array1 );
-        gumballMachine.turnCrank();
+        gumballMachine1.insertQuarter( array1 );
+        gumballMachine1.turnCrank();
 
-        System.out.println(gumballMachine);
+        System.out.println(gumballMachine2);
+        
+        gumballMachine2.insertQuarter( array2 );
+        gumballMachine2.turnCrank();
 
-        gumballMachine.insertQuarter( array2 );
-        gumballMachine.turnCrank();
-        gumballMachine.insertQuarter( array3 );
-        gumballMachine.turnCrank();
+        System.out.println(gumballMachine3);
+        gumballMachine3.insertQuarter( array3 );
+        gumballMachine3.turnCrank();
 
-        System.out.println(gumballMachine);
+        
     }
 }
