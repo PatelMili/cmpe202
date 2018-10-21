5a7b5fc2f1b3c1d146f08ab00bff89b0f39d59b3
PatelMili <mili.patel@sjsu.edu>
2018-10-20 20:31:07 -0700

    lab 7 updating counter burger code to match new menu

---
+++ lab7/counter-burger/source-code/Sides.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Sides here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Sides extends LeafDecorator
+{
+   private String[] options ;
+    
+    
+    public Sides( String d )
+    {
+        super(d) ;
+    }
+    
+    public void setOptions( String[] options )
+    {
+        this.options = options ;
+        if ( options.length > 0 )
+            this.price += (options.length) * 3.00 ;
+    }
+    
+    public String getDescription() 
+    {
+        String desc = "   " ;
+        for ( int i = 0; i<options.length; i++ )
+        {
+            if (i>0) desc += " + " + options[i] ;
+            else desc = options[i] ;
+        }        
+        return desc ;
+    }
+}
